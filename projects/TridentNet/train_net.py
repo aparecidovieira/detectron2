36b6184e27b226712f7b394caff0fdf33209c3f7
Cesar Duarte <cesaraparecido3@gmail.com>
2021-05-19 11:21:08 +0900

    Update train_net.py

--- projects/TridentNet/train_net.py
+++ projects/TridentNet/train_net.py
@@ -9,22 +9,28 @@ This script is a simplified version of the training script in detectron2/tools.
 
 import os
 import urllib.request
-
+import detectron2.data.transforms as T
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, launch
 from detectron2.evaluation import COCOEvaluator
-import torch
+from detectron2.structures import BoxMode
+import json, torch
+import random
+import os
+from detectron2.data import DatasetCatalog, MetadataCatalog
+
 from tridentnet import add_tridentnet_config
+os.environ['CUDA_VISIBLE_DEVICES'] = '2, 3'
 
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu' )
 class Trainer(DefaultTrainer):
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
         if output_folder is None:
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
         return COCOEvaluator(dataset_name, cfg, True, output_dir=output_folder)
-    
+
 # class CocoTrainer(DefaultTrainer):
 
 #   @classmethod
@@ -37,119 +43,134 @@ class Trainer(DefaultTrainer):
 #     return COCOEvaluator(dataset_name, cfg, False, output_folder)
 
 
+class CustomTrainer(DefaultTrainer):
+    @classmethod
+    def build_train_loader(cls, cfg):
+        return build_detection_train_loader(cfg, mapper=custom_mapper)
+
+
+weights_catalog = {'R-50-GN': "https://dl.fbaipublicfiles.com/detectron/ImageNetPretrained/47261647/",
+                'R-50.pkl': "https://dl.fbaipublicfiles.com/detectron2/ImageNetPretrained/MSRA/"}
+
+
+def custom_mapper(dataset_dict):
+    # it will be modified by code below
+    dataset_dict = copy.deepcopy(dataset_dict)
+    image = utils.read_image(dataset_dict["file_name"], format="BGR")
+    transform_list = [
+        T.Resize((512, 512)),
+        T.RandomBrightness(0.8, 1.8),
+        T.RandomContrast(0.6, 1.3),
+        T.RandomSaturation(0.8, 1.4),
+        T.RandomRotation(angle=[90, 90]),
+        T.RandomLighting(0.7),
+        # T.RandomFlip(prob=0.4, horizontal=False, vertical=True),
+    ]
+    image, transforms = T.apply_transform_gens(transform_list, image)
+    dataset_dict["image"] = torch.as_tensor(
+        image.transpose(2, 0, 1).astype("float32"))
+
+    annos = [
+        utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+        for obj in dataset_dict.pop("annotations")
+        # if obj.get("iscrowd", 0) == 0
+    ]
+    instances = utils.annotations_to_instances(annos, image.shape[:2])
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dict
+
+
 def setup(args):
     """
     Create configs and perform basic setups.
     """
-    from detectron2.structures import BoxMode
-    import json, random, os
-#     '0' 152 
-    dic_marks = {'0': 'No_right_turn',
-    '1':'No_left_turn',
-    '2':'Prohibited_straight',
-    '3':'Straight_left_turn_prohibited',
-    '4':'Go_straight_not_turn_right',
-    '5':'Left_right_turn_prohibited',
-    '6':'No_U-turn',
-    '7':'Speed_limit',
-    '8':'Speed_limit_child_zone',
-    '9':'Concession',
-    '10':'Crosswalk',
-    '11':'Bike_Priority_Road',
-    '12':'Turn_left',
-    '13':'Go_straight',
-    '14':'Turn_right',
-    '15':'Turn_left_go_straight',
-    '16':'Go_straight_turn_right',
-    '17':'U-turn',
-    '18':'U-turn-left_turn',
-    '19':'Unprotected_Left_Turn'}
-#     dic_marks = {'0': 'Crosswalk'}
+    # cfg = get_cfg()
+    dataset_path = '/raid/cesar_workspace/cesar_workspace/Object_Detection/Detectron2/detectron2/detectron2/data/Datasets/'
+    train = dataset_path + "up_trees_train_2021"
+    val = dataset_path + "up_trees_val_2021"
+    train_dataset = train  # cfg.DATASETS.TRAIN
+    val_dataset = val  # cfg.DATASETS.TEST
+    # print(cfg.DATASETS.TRAIN, 'eee')
+    dic_marks_path = dataset_path + "up_trees_labels.json"
+    datasets_dic = {'train': train_dataset, 'val': val_dataset}
+    dic_marks = {'0':'up_tree'}
+#     with open(dic_marks_path, 'w') as out:
+#         json.dump(dic_marks, out)
+    with open(dic_marks_path, 'r') as out:
+        dic_marks = json.load(out)
     # cat_ids = {label:key for key, label in dic_marks.items()}
     classes = [label for key, label in dic_marks.items()]
-    base = '/media/hdd/cesar_workspace/Object_Detection/Detectron2/detectron2/detectron2/data/Datasets/'
 
     def get_board_dicts(imgdir):
-        json_file = imgdir + '.json'#+"/dataset.json" #Fetch the json file
+        json_file = imgdir + '.json'  # Fetch the json file
         print(json_file)
         with open(json_file) as f:
             dataset_dicts = json.load(f)
         for i in dataset_dicts:
-            filename = i["file_name"] 
+            filename = i["file_name"]
             for j in i["annotations"]:
-                j["bbox_mode"] = BoxMode.XYWH_ABS #Setting the required Box Mode
+                # Setting the required Box Mode
+                j["bbox_mode"] = BoxMode.XYWH_ABS
                 j["category_id"] = int(j["category_id"])
         return dataset_dicts
 
+    # Registering the Dataset
 
-    from detectron2.data import DatasetCatalog, MetadataCatalog
-    
-    #Registering the Dataset
-    data_type = '20k_NGI'
     for d in ['val', 'train']:
-        DatasetCatalog.register("lanes_%s_%s"%(d, data_type), lambda d=d: get_board_dicts(base + "lanes_%s_%s"%(d, data_type)))
-        MetadataCatalog.get("lanes_%s_%s"%(d, data_type)).set(thing_classes=classes)
-        
-    board_metadata = MetadataCatalog.get("lanes_train_%s"%data_type)
-    dataset_dicts = get_board_dicts(base + "lanes_train_%s"%data_type)
+        # print(datasets_dic[d])
+        dataset_name = os.path.basename(datasets_dic[d])
+        print(dataset_name)
+
+        DatasetCatalog.register(dataset_name, lambda d=d: get_board_dicts(
+            datasets_dic[d]))
+        MetadataCatalog.get(dataset_name).set(thing_classes=classes)
+
+    train_name = os.path.basename(datasets_dic['train'])
+    val_name = os.path.basename(datasets_dic['val'])
+    print(train_name, val_name)
+    board_metadata = MetadataCatalog.get(train_name)
+    dataset_dicts = get_board_dicts(train_dataset)
     n_imgs = len(dataset_dicts)
-    
-    dataset_dicts = get_board_dicts(base + "lanes_val_%s"%data_type)
-    n_img_test = len(dataset_dicts)
-    
-    print('Number of images on training data is :',n_imgs, n_img_test)
+    dataset_dicts = get_board_dicts(val_dataset)
+    n_imgs_val = len(dataset_dicts)
+    print('Number of images on training data is :', n_imgs)
     cfg = get_cfg()
     add_tridentnet_config(cfg)
+
+#     cfg.DATALOADER.NUM_WORKERS = 2
+
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
-    cfg.DATALOADER.NUM_WORKERS = 2
-#     cfg.SOLVER.IMS_PER_BATCH = 2
-#     cfg.SOLVER.MAX_ITER = 00
-#     cfg.SOLVER.BASE_LR = 0.002
-    
-#     cfg.SOLVER.MAX_ITER = int(n_imgs//cfg.SOLVER.IMS_PER_BATCH * 80)  #No. of iterations   
-    cfg.DATASETS.TRAIN = ("lanes_train_%s"%(data_type),)
-    cfg.DATASETS.TEST = ("lanes_val_%s"%(data_type),)
-    cfg.TEST.EVAL_PERIOD =  int(n_imgs//cfg.SOLVER.IMS_PER_BATCH) # No. of iterations after which the Validation Set is evaluated. 
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128  
-#     cfg.NUM_GPUS = 2
-#     cfg.MODEL.DEVICE='cuda:1'
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(classes) # No. of classes = [HINDI, ENGLISH, OTHER]
-    print(cfg.SOLVER.MAX_ITER)
-#     return None
-    path = '/media/hdd/cesar_workspace/Object_Detection/Detectron2/detectron2/ImageNetPretrained/MSRA/'
-#     if 'X-101' in path or '152' in path:
-#         path+='FAIR/'
+
+    cfg.DATASETS.TRAIN = (train_name,)
+    cfg.DATASETS.TEST = (val_name,)
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+
+    # No. of iterations after which the Validation Set is evaluated.
+    cfg.TEST.EVAL_PERIOD = (n_imgs//cfg.SOLVER.IMS_PER_BATCH)
+
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(classes)
+
+    path = '/raid/cesar_workspace/cesar_workspace/Object_Detection/Detectron/detectron2/ImageNetPretrained/MSRA/'
     os.makedirs(path, exist_ok=True)
     backbone = os.path.basename(cfg.MODEL.WEIGHTS)
-    print('Number of images on training data is :',n_imgs, n_img_test)
-    
-#    "detectron2://ImageNetPretrained/MSRA/R-101.pkl"
+    print('Number of images on training data is :', n_imgs, n_imgs_val)
+    backbone += '.pkl' if '.pkl' not in backbone else ''
     weight = path + backbone
-#     print(weight)
+    print(weight)
     if not os.path.isfile(weight):
-        url_weights = 'https://dl.fbaipublicfiles.com/detectron2/' + cfg.MODEL.WEIGHTS.replace('detectron2://', '')
-        print(url_weights)      
+        print("Downloading ImageNet weights")
+        url_weights = weights_catalog[backbone] + backbone 
         urllib.request.urlretrieve(url_weights, weight)
-#         url_weights = 'https://dl.fbaipublicfiles.com/detectron2/ImageNetPretrained/MSRA/R-50.pkl'
-    weight = '/media/hdd/cesar_workspace/Object_Detection/Detectron2/detectron2/projects/TridentNet/output-101/model_0179999.pth'
-#     weight = '/media/hdd/cesar_workspace/Object_Detection/Detectron2/detectron2/ImageNetPretrained/MSRA/R-50-GN.pkl'
+
     cfg.MODEL.WEIGHTS = weight
-    print(cfg.MODEL.WEIGHTS, 'Loading weights like a Jedi')
-#     cfg.MODEL.WEIGHTS = './output_20k_X/model_final.pth'
+    print(weight)
 
-#     cfg.MODEL.WEIGHTS = '/media/hdd/cesar_workspace/Object_Detection/Detectron2/detectron2/ImageNetPretrained/MSRA/R-50.pkl'
-#     cfg.SOLVER.STEPS: (3000, 6000)
-#     cfg.SOLVER.MAX_ITER = 150000
-        
-#     cfg.OUTPUT_DIR = './output-101/'
-#     cfg.OUTPUT_DIR = './output_20k_X-152/'#%data_type
-#     cfg.MODEL.WEIGHTS = cfg.OUTPUT_DIR + 'model_0144999.pth'
-#     args.resume = True
-    default_setup(cfg, args)    
+    # cfg.OUTPUT_DIR = './output_%s_X-101_b/'%accr
+    print(cfg, '~~ I dedicate this to Shadow Moon ~~')
+
+    default_setup(cfg, args)
     cfg.freeze()
-   
-
     return cfg
 
 
@@ -158,23 +179,17 @@ def main(args):
 
     if args.eval_only:
         model = Trainer.build_model(cfg)
-#         PATH = "./entire_model.pt"
-        
-# # Save
-#         torch.save(model, PATH)
-        
         DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
             cfg.MODEL.WEIGHTS, resume=args.resume
         )
         res = Trainer.test(cfg, model)
-
         return res
-    
+
     trainer = Trainer(cfg)
     trainer.resume_or_load(resume=args.resume)
     return trainer.train()
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1,2'
+
 if __name__ == "__main__":
     args = default_argument_parser().parse_args()
     print("Command Line Args:", args)
